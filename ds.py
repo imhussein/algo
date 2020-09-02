8b6b3eae81ef50f7978cb06dbd8e083773a849b1
mh10445 <hussein@blockgemini.com>
2020-09-02 14:03:17 +0400

    fix issues

--- ds.py
+++ ds.py
@@ -1,6 +1,3 @@
-# Single Linked List Implementation
-
-
 class Node:
     def __init__(self, value):
         self.value = value
@@ -9,45 +6,19 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        self.len = 0
         self.head = None
         self.tail = None
+        self.len = 0
 
-    def append(self, value):
+    def push(self, value):
         new_node = Node(value)
         if not self.head:
-            self.tail = self.head = new_node
+            self.head = self.tail = new_node
         else:
             self.tail.next = new_node
             self.tail = new_node
         self.len += 1
         return self
-
-    def pop(self):
-        if not self.head:
-            return None
-        new_tail = current = self.head
-        while current.next:
-            new_tail = current
-            current = current.next
-        self.tail = new_tail
-        self.tail.next = None
-        self.len -= 1
-        if self.len == 0:
-            self.head = None
-            self.tail = None
-        return current
-
-    def shift(self):
-        if not self.head:
-            return None
-        current_head = self.head
-        self.head = current_head.next
-        self.len -= 1
-        if self.len == 0:
-            self.head = None
-            self.tail = None
-        return current_head
 
     def unshift(self, value):
         new_node = Node(value)
@@ -56,18 +27,44 @@ class LinkedList:
         else:
             new_node.next = self.head
             self.head = new_node
-            self.len += 1
-            return self
+        self.len += 1
+        return self
+
+    def pop(self):
+        if not self.head:
+            return None
+        else:
+            current = self.head
+            new_tail = current
+            while current.next:
+                new_tail = current
+                current = current.next
+            self.tail = new_tail
+            self.tail.next = None
+            self.len -= 1
+            if self.len == 0:
+                self.head = None
+                self.tail = None
+            return current
+
+    def shift(self):
+        if not self.head:
+            return None
+        else:
+            head = self.head
+            self.head = head.next
+        self.len -= 1
+        return head
 
     def get(self, index):
-        if index < 0 or index >= self.len:
+        if self.len == 0 or index >= self.len:
             return None
-        counter = 0
-        current = self.head
-        while counter != index:
-            current = current.next
-            counter += 1
-        return current
+        current = 0
+        node = self.head
+        while index != current:
+            node = node.next
+            current += 1
+        return node
 
     def set(self, index, value):
         node = self.get(index)
@@ -80,27 +77,27 @@ class LinkedList:
         if index < 0 or index > self.len:
             return False
         if index == self.len:
-            return not(not(self.append(value)))
+            return not(not(self.push(value)))
         if index == 0:
             return not(not(self.unshift(value)))
         new_node = Node(value)
-        prev = self.get(index)
-        temp = prev.next
-        prev.next = new_node
+        node = self.get(index - 1)
+        temp = node.next
+        node.next = new_node
         new_node.next = temp
         self.len += 1
-        return True
+        return self
 
     def remove(self, index):
-        if index < 0 or index > self.len:
+        if self.len < 0 or self.len < index:
             return False
+        if index == self.len - 1:
+            self.pop()
         if index == 0:
-            return self.shift()
-        if index == self.len:
-            return self.pop()
-        prev_node = self.get(index - 1)
-        removed = prev_node.next
-        prev_node.next = removed.next
+            self.shift()
+        prev = self.get(index - 1)
+        removed = prev.next
+        prev.next = removed.next
         self.len -= 1
         return removed
 
