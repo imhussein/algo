255e1060594ce07efad137e0e26b33b8cf9bcf7d
mh10445 <hussein@blockgemini.com>
2020-09-20 18:47:21 +0400

    add code

--- internals/app.js
+++ internals/app.js
@@ -1,7 +1,23 @@
 const crypto = require("crypto");
-process.env.UV_THREADPOOL_SIZE = 1;
+process.env.UV_THREADPOOL_SIZE = 4;
+const https = require("https");
+const fs = require("fs");
 
 var initialTime = Date.now();
+
+https
+  .request("https://google.com", (res) => {
+    res
+      .on("data", (data) => {})
+      .on("end", () => {
+        console.log("Google", Date.now() - initialTime);
+      });
+  })
+  .end();
+
+fs.readFile("mohamed.txt", { encoding: "utf-8" }, (err, data) => {
+  console.log(data);
+});
 
 crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
   console.log(`1 - ) ${Date.now() - initialTime}`);
@@ -17,8 +33,4 @@ crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
 
 crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
   console.log(`4 - ) ${Date.now() - initialTime}`);
-});
-
-crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-  console.log(`5 - ) ${Date.now() - initialTime}`);
 });
