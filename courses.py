112cf40ee41bb57bf8c94bfd7835eddda1508d36
mh10445 <hussein@blockgemini.com>
2020-10-21 18:41:53 +0400

    add code

--- courses.py
+++ courses.py
@@ -1,13 +1,13 @@
-# 1 MERN eCommerce From Scratch
+# 1 MERN eCommerce From Scratch (60%)
 # 2 Docker and Kubernetes: The Complete Guide
 # 3 Microservices with Node JS and React
 # 4 Introduction to Data Visualization with d3.js v4
 # 5 Data Visualization for React Developers
 # 6 Building Custom Data Visualizations
 # 7 Introduction to D3.js
-# 8 Server-Side GraphQL in Node.js
+# 8 Server-Side GraphQL in Node.js (90%)
 # 9 Client-Side GraphQL in React
-# 10 Adv9anced GraphQL, v2
+# 10 Advanced GraphQL, v2
 # 11 Node.js, Express, MongoDB & More: The Complete Bootcamp 2020
 # 12 Digging Into Node.js
 # 13 Advanced CSS and Sass: Flexbox, Grid, Animations and More!
---
+++ coursestowatch/Server-Side GraphQL in Node.js/demo.js
@@ -0,0 +1,120 @@
+const gql = require("graphql-tag");
+const { ApolloServer } = require("apollo-server");
+require("colors");
+const mongoose = require("mongoose");
+
+async function connectToMongo(uri) {
+  try {
+    const conn = await mongoose.connect(uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    return conn;
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+connectToMongo("mongodb://brad:Mohamedbrad1@ds031877.mlab.com:31877/brad")
+  .then((conn) =>
+    console.log(`DB Connected on host ${conn.connection.host}`.green.bold)
+  )
+  .catch((err) => {
+    console.log(err);
+  });
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+const models = { User };
+
+const typeDefs = gql`
+  interface HasID {
+    id: ID
+  }
+
+  type User implements HasID {
+    id: ID
+    name: String!
+    email: String!
+    password: String!
+    createdAt: String
+    updatedAt: String
+    isAdmin: Boolean
+  }
+
+  type Query {
+    users: [User!]!
+    user(id: ID): User!
+    admin(userInput: UserInput): User
+  }
+
+  input UserInput {
+    id: ID
+  }
+`;
+
+async function getUser(_id) {
+  const user = await User.findOne({ _id });
+  return user;
+}
+
+const resolvers = {
+  Query: {
+    users: async (parentValue, args, { models: { User } }, info) => {
+      const users = await User.find();
+      return users;
+    },
+    user: (parentValue, { id: _id }, { models: { User } }, info) => {
+      return getUser(_id);
+    },
+    admin: (
+      parentValue,
+      { userInput: { id: _id } },
+      { models: { User } },
+      info
+    ) => {
+      return getUser(_id);
+    },
+  },
+  User: {
+    id: async (parentValue, args, { models: { User } }, info) => {
+      return parentValue._id;
+    },
+    name: () => "Always Mohamed Hussein is The Name",
+  },
+  HasID: {
+    __resolveType(parentValue) {
+      if (parentValue.isAdmin) return "User";
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: (req) => {
+    return {
+      models,
+    };
+  },
+});
+
+server.listen(4000, () =>
+  console.log(`GraphQL Server Listening On Port ${4000}`.yellow.bold)
+);
